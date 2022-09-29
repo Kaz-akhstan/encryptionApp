61d52fa4a208b2f60df6fc3c6bb608bde9970b3b
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2022-09-29 15:02:10 +0200

    Commit

--- src/model.java
+++ src/model.java
@@ -1,3 +1,88 @@
-public class model {
+import java.io.*;
+import java.util.ArrayList;
 
+public class model {
+    String filename;
+    String messageInput;
+    String message;
+    String keyFile;
+    String keyInput;
+    String key;
+    String encryptedMessage;
+
+    public String readMessageFromFile(String filename)
+    {
+        String message = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            message = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return message;
+    }
+
+    public String readMessageFromInput(String messageInput)
+    {
+        return messageInput;
+    }
+
+    public String readMessageFromEncryptedFile(String filename)
+    {
+        return null; // FIXME: 2022-09-29
+    }
+
+    public String readKeyFromFile(String keyFile)
+    {
+        String key = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(keyFile));
+            key = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return key;
+    }
+
+    public String readKeyFromInput(String keyInput)
+    {
+        return keyInput;
+    }
+
+    public String readKeyFromEncryptedFile(String keyFile)
+    {
+        return null; // FIXME: 2022-09-29
+    }
+
+    public String encryptMessage(String message, String key)
+    {
+        String encrypted = "";
+        int[] encryptedMessage = new int[message.length()];
+        int[] keyList = new int[key.length()];
+        for (int i = 0; i < keyList.length; i++) {
+            keyList[i] = key.charAt(i);
+        }
+        ArrayList<Integer>intList = new ArrayList<>();
+        for (int i = 0; i < message.length(); i++) {
+            encryptedMessage[i] = message.charAt(i)^Integer.parseInt(key);
+            encrypted += (char) encryptedMessage[i];
+        }
+        return encrypted;
+    }
+
+    public String decryptMessage(String encryptedMessage)
+    {
+        return null;
+    }
+
+    public void outputToFile(String filename, String encryptedMessage)
+    {
+        DataOutputStream dos;
+        try {
+            dos = new DataOutputStream(new FileOutputStream(filename));
+            dos.writeInt(Integer.parseInt(encryptedMessage));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        String s = "";
+        String k = "";
+        String e = "";
+        Scanner sc = new Scanner(System.in);
+        model m = new model();
+        System.out.println("Message: ");
+        s = m.readMessageFromInput(sc.nextLine());
+        System.out.println("Key: ");
+        k = m.readKeyFromInput(sc.nextLine());
+        System.out.println("Encrypting");
+        e = m.encryptMessage(s, k);
+        System.out.println(e);
+        System.out.println("Output to file: ");
+        m.outputToFile(sc.nextLine(), e);
+    }
+}
