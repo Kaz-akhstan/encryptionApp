5470ec9e53de7b5b5f7fc875d19418406d5df499
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2022-10-14 12:47:39 +0200

    Commit före allt går sönder 2

--- src/view.java
+++ src/view.java
@@ -5,6 +5,17 @@ public class view {
     private JTextField Filepath;
     private JRadioButton fromFileRadioButton;
     private JPanel panel;
+    private JRadioButton fromInput;
+    private JTextField inputField;
+    private JButton send1;
+    private JLabel enterMsg;
+    private JLabel enterKey;
+    private JRadioButton fromFile2;
+    private JTextField filePath2;
+    private JButton browse2;
+    private JRadioButton fromInput2;
+    private JTextField fromKey2;
+    private JButton send2;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
